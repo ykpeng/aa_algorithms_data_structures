cca8755005badc94816ddfc4695d43ead61b15e6
Yi-Ke Peng <yi.ke.peng@gmail.com>
2016-08-04 15:04:18 -0700

    added dijkstra

--- dijkstras_algorithm/lib/dijkstra2.rb
+++ dijkstras_algorithm/lib/dijkstra2.rb
@@ -3,4 +3,29 @@ require_relative 'priority_map'
 
 # O(|V| + |E|*log(|V|)).
 def dijkstra2(source)
+  shortest_paths = {}
+  possible_paths = PriorityMap.new do |data1, data2|
+    data1[:cost] <=> data2[:cost]
+  end
+  possible_paths[source] = { cost: 0, last_edge: nil }
+
+  while !possible_paths.empty?
+    vertex, data = possible_paths.extract
+    min_cost = data[:cost]
+    shortest_paths[vertex] = data
+    vertex.out_edges.each do |edge|
+      if !shortest_paths.include?(edge.to_vertex)
+        relax(vertex, edge, possible_paths, min_cost)
+      end
+    end
+  end
+
+  shortest_paths
+end
+
+def relax(vertex, edge, possible_paths, min_cost)
+  to_vertex = edge.to_vertex
+  if !possible_paths[to_vertex] || possible_paths[to_vertex][:cost] > min_cost + edge.cost
+    possible_paths[to_vertex] = { cost: min_cost + edge.cost, last_edge: edge }
+  end
 end
--- dijkstras_algorithm/lib/priority_map.rb
+++ dijkstras_algorithm/lib/priority_map.rb
@@ -2,32 +2,53 @@ require_relative 'heap2'
 
 class PriorityMap
   def initialize(&prc)
+    @map = {}
+    @queue = BinaryMinHeap.new do |key1, key2|
+      prc.call(@map[key1], @map[key2])
+    end
   end
 
   def [](key)
+    @map[key]
   end
 
   def []=(key, value)
+    if !@map.include?(key)
+      insert(key, value)
+    else
+      update(key, value)
+    end
   end
 
   def count
+    @queue.count
   end
 
   def empty?
+    @queue.empty?
   end
 
   def extract
+    vertex = @queue.extract
+    data = @map[vertex]
+    @map.delete(vertex)
+    [vertex, data]
   end
 
   def has_key?(key)
+    !!@map[key]
   end
 
   protected
   attr_accessor :map, :queue
 
   def insert(key, value)
+    @map[key] = value
+    @queue.push(key)
   end
 
   def update(key, value)
+    @map[key] = value
+    @queue.reduce!(key)
   end
 end
